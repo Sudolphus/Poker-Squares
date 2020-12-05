cd74170281ab7c0937c8f887ca84e146d2d2d2f9
Micheal Hansen <micheal.hansen@gmail.com>
2020-12-05 03:03:04 -0800

    change up the images because the first pack didn't have face cards

--- src/CardSlot/index.js
+++ src/CardSlot/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const CardSlot = ({ row, column, card }) => {
   const { rank, suit } = card;
+  const imageName = `${rank}${suit}`.toLowerCase();
 
   return (
     <div 
       className = 'cardSlot' 
       id = { `card${row}x${column}` }>
-      <p>Rank: { rank }</p>
-      <p>Suit: { suit } </p>
+      <img src={require(`./../Images/${imageName}.png`)} alt={`${rank} of ${suit}`}/>
     </div>
   )
 }
--- src/Deck/deck.js
+++ src/Deck/deck.js
@@ -1,210 +1,264 @@
+import * as c from './images';
+
 export const deck = [
   {
     rank: 'Ace',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.AceHearts
   },
   {
     rank: 'King',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.KingHearts
   },
   {
     rank: 'Queen',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.QueenHearts
   },
   {
     rank: 'Jack',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.JackHearts
   },
   {
     rank: 'Ten',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.TenHearts
   },
   {
     rank: 'Nine',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.NineHearts
   },
   {
     rank: 'Eight',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.EightHearts
   },
   {
     rank: 'Seven',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.SevenHearts
   },
   {
     rank: 'Six',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.SixHearts
   },
   {
     rank: 'Five',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.FiveHearts
   },
   {
     rank: 'Four',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.FourHearts
   },
   {
     rank: 'Three',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.ThreeHearts
   },
   {
     rank: 'Two',
-    suit: 'Hearts'
+    suit: 'Hearts',
+    image: c.TwoHearts
   },
   {
     rank: 'Ace',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.AceSpades
   },
   {
     rank: 'King',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.KingSpades
   },
   {
     rank: 'Queen',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.QueenSpades
   },
   {
     rank: 'Jack',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.JackSpades
   },
   {
     rank: 'Ten',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.TenSpades
   },
   {
     rank: 'Nine',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.NineSpades
   },
   {
     rank: 'Eight',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.EightSpades
   },
   {
     rank: 'Seven',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.SevenSpades
   },
   {
     rank: 'Six',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.SixSpades
   },
   {
     rank: 'Five',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.FiveSpades
   },
   {
     rank: 'Four',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.FourSpades
   },
   {
     rank: 'Three',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.ThreeSpades
   },
   {
     rank: 'Two',
-    suit: 'Spades'
+    suit: 'Spades',
+    image: c.TwoSpades
   },
   {
     rank: 'Ace',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.AceDiamonds
   },
   {
     rank: 'King',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.KingDiamonds
   },
   {
     rank: 'Queen',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.QueenDiamonds
   },
   {
     rank: 'Jack',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.JackDiamonds
   },
   {
     rank: 'Ten',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.TenDiamonds
   },
   {
     rank: 'Nine',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.NineDiamonds
   },
   {
     rank: 'Eight',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.EightDiamonds
   },
   {
     rank: 'Seven',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.SevenDiamonds
   },
   {
     rank: 'Six',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.SixDiamonds
   },
   {
     rank: 'Five',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.FiveDiamonds
   },
   {
     rank: 'Four',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.FourDiamonds
   },
   {
     rank: 'Three',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.ThreeDiamonds
   },
   {
     rank: 'Two',
-    suit: 'Diamonds'
+    suit: 'Diamonds',
+    image: c.TwoDiamonds
   },
   {
     rank: 'Ace',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.AceClubs
   },
   {
     rank: 'King',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.KingClubs
   },
   {
     rank: 'Queen',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.QueenClubs
   },
   {
     rank: 'Jack',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.JackClubs
   },
   {
     rank: 'Ten',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.TenClubs
   },
   {
     rank: 'Nine',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.NineClubs
   },
   {
     rank: 'Eight',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.EightClubs
   },
   {
     rank: 'Seven',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.SevenClubs
   },
   {
     rank: 'Six',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.SixClubs
   },
   {
     rank: 'Five',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.FiveClubs
   },
   {
     rank: 'Four',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.FourClubs
   },
   {
     rank: 'Three',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.ThreeClubs
   },
   {
     rank: 'Two',
-    suit: 'Clubs'
+    suit: 'Clubs',
+    image: c.TwoClubs
   },
 ]
