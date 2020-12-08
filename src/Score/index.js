d36f25945a8c01910b43aaa47f94889bca089ba7
Micheal Hansen <micheal.hansen@gmail.com>
2020-12-07 23:10:13 -0800

    further improvements to algorithm

--- src/Score/index.js
+++ src/Score/index.js
@@ -1,24 +1,25 @@
 import ScoreCard from './scoreCard';
 import isStraight from './straight';
 import isFlush from './flush';
-import pairs from './pairs';
 
 const Score = hand => {
   const ranks = hand.map(card => card.rank).sort();
   const straight = isStraight(ranks);
   const flush = isFlush(hand);
   const fourOfAKind = ranks[0] === ranks[3] || ranks[1] === ranks[4];
+  const fullHouse = (ranks[0] === ranks[1] && ranks[2] === ranks[4]) || (ranks[0] === ranks[2] && ranks[3] === ranks[4]);
   const threeOfAKind = ranks[0] === ranks[2] || ranks[1] === ranks[3] || ranks[2] === ranks[4];
-  const numPairs = pairs(ranks);
+  const twoPair = (ranks[0] === ranks[1] && ranks[2] === ranks[3]) || (ranks[1] === ranks[2] && ranks[3] === ranks[4]) || (ranks[0] === ranks[1] && ranks[3] === ranks[4]);
+  const onePair = ranks[0] === ranks[1] || ranks[1] === ranks[2] || ranks[2] === ranks[3] || ranks[3] === ranks[4];
 
   let [name, points] = [null, null];
-  if (straight && flush && ranks[4] === 'Ace') {
+  if (straight && flush && ranks.includes('Ace') && ranks.includes('King')) {
     [name, points] = ['Royal Flush', 100];
   } else if (straight && flush) {
     [name, points] = ['Straight Flush', 75];
   } else if (fourOfAKind) {
     [name, points] = ['Four of a Kind', 50];
-  } else if (threeOfAKind && numPairs === 1) {
+  } else if (fullHouse) {
     [name, points] = ['Full House', 25];
   } else if (flush) {
     [name, points] = ['Flush', 20];
@@ -26,9 +27,9 @@ const Score = hand => {
     [name, points] = ['Straight', 15];
   } else if (threeOfAKind) {
     [name, points] = ['Three of a Kind', 10];
-  } else if (numPairs === 2) {
+  } else if (twoPair) {
     [name, points] = ['Two Pair', 5];
-  } else if (numPairs === 1) {
+  } else if (onePair) {
     [name, points] = ['One Pair', 2];
   } else {
     [name, points] = ['Nothing', 0];
